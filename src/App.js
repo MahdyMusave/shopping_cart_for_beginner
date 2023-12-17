0a80db1547c074449286cad53d49045c4b6c11cb
mahde13 <musavemahdy>
2023-12-17 01:20:19 -0900

    added style for navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./component/navbar";
 import Home from "./page/home";
+import Cart from "./component/cart";
+import About from "./page/about";
+import Contact from "./page/contact";
+import "./app.css";
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </>
